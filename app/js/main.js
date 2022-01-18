7bbc7007c89895f2f46fc200720a8cd2b6fb2ff5
Iurii Manastyrskyi <ternoboy@gmail.com>
2022-01-18 11:12:54 -0500

    added responsive design part3 for index.html Final

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,10 @@ $(function () {
     $('.header__btn-menu').on('click', function(){
         $('.menu').toggleClass('menu--open');
     });
+
+    if($(window).width() < 651){
+        $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
+    }
    
     $('.top__slider').slick({
         dots: true,
